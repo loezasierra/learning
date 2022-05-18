fa82d5914e5893ba182c86ffaa0aba1fd72abc8c
loezasierra <joseloeza@gmail.com>
2022-05-18 12:23:09 -0700

    Included Windows specific functions for portability

--- books/buildyourownlisp/prompt.c
+++ books/buildyourownlisp/prompt.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If compiling on Windows, compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+// Fake readline function
+char *readline(char *prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+// Fake add_history function
+void add_history(char *unused) {}
+
+
+/* Otherwise include editline header */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
     // Print Version and Exit Information
-    puts("Lispy Version 0.0.0.0.2");
+    puts("Lispy Version 0.0.0.0.3");
     puts("Press Ctrl+C to Exit\n");
 
     // In a never ending loop

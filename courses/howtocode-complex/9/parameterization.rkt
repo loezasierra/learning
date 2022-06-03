c66394157bc775f63dd31fac9d8e5b0f1f3a5420
loezasierra <joseloeza@gmail.com>
2022-06-02 18:06:10 -0700

    Added abstraction lecture examples

---
+++ courses/howtocode-complex/9/parameterization.rkt
@@ -0,0 +1,137 @@
+#lang racket
+
+;; parameterization-starter.rkt
+
+(define (area r)
+  (* pi (sqr r)))
+
+(area 4) ; (* pi (sqr 4)) ;area of circle radius 4
+(area 6); (* pi (sqr 6)) ;area of circle radius 6
+
+
+;; ====================
+
+;; ListOfString -> Boolean
+;; produce true if los includes "UBC"
+(check-expect (contains-ubc? empty) false)
+(check-expect (contains-ubc? (cons "McGill" empty)) false)
+(check-expect (contains-ubc? (cons "UBC" empty)) true)
+(check-expect (contains-ubc? (cons "McGill" (cons "UBC" empty))) true)
+
+;(define (contains-ubc? los) false) ;stub
+
+;<template from ListOfString>
+
+(define (contains-ubc? los) (contains? "UBC" los))
+
+;; ListOfString -> Boolean
+;; produce true if los includes "McGill"
+(check-expect (contains-mcgill? empty) false)
+(check-expect (contains-mcgill? (cons "UBC" empty)) false)
+(check-expect (contains-mcgill? (cons "McGill" empty)) true)
+(check-expect (contains-mcgill? (cons "UBC" (cons "McGill" empty))) true)
+
+;(define (contains-mcgill? los) false) ;stub
+
+;<template from ListOfString>
+
+(define (contains-mcgill? los) (contains? "McGill" los))
+
+
+;; String (listof String) -> Bool
+;; return true if los includes s
+(check-expect (contains? "UBC" empty) false)
+(check-expect (contains? "UBC" (cons "McGill" empty)) false)
+(check-expect (contains? "UBC" (cons "UBC" empty)) true)
+(check-expect (contains? "UBC" (cons "McGill" (cons "UBC" empty))) true)
+(check-expect (contains? "UBC" (cons "UBC" (cons "McGill" empty))) true)
+(check-expect (contains? "Toronto" (cons "UBC" (cons "McGill" empty))) false)
+
+(define (contains? s los)
+  (cond [(empty? los) false]
+        [else
+         (if (string=? (first los) s)
+             true
+             (contains? s (rest los)))]))
+
+
+;; ====================
+
+;; ListOfNumber -> ListOfNumber
+;; produce list of sqr of every number in lon
+(check-expect (squares empty) empty)
+(check-expect (squares (list 3 4)) (list 9 16))
+
+;(define (squares lon) empty) ;stub
+
+;<template from ListOfNumber>
+
+(define (squares lon) (map2 sqr lon))
+
+;; ListOfNumber -> ListOfNumber
+;; produce list of sqrt of every number in lon
+(check-expect (square-roots empty) empty)
+(check-expect (square-roots (list 9 16)) (list 3 4))
+
+;(define (square-roots lon) empty) ;stub
+
+;<template from ListOfNumber>
+
+(define (square-roots lon) (map2 sqrt lon))
+
+
+;; (X -> Y) (listof X) -> (listof Y)
+;; given fn and (list n0 n1 ...) return (list (fn n0) (fn n1) ...)
+(check-expect (map2 sqr empty) empty)
+(check-expect (map2 sqr (list 2 4)) (list 4 16))
+(check-expect (map2 sqrt (list 16 9)) (list 4 3))
+(check-expect (map2 abs (list 2 -3 4)) (list 2 3 4))
+(check-expect (map2 string-length (list "hi" "bye")) (list 2 3))
+
+(define (map2 fn lon)
+  (cond [(empty? lon) empty]
+        [else
+         (cons (fn (first lon))
+               (map2 fn (rest lon)))]))
+
+
+;; ====================
+
+;; ListOfNumber -> ListOfNumber
+;; produce list with only positive? elements of lon
+(check-expect (positive-only empty) empty)
+(check-expect (positive-only (list 1 -2 3 -4)) (list 1 3))
+
+;(define (positive-only lon) empty) ;stub
+
+;<template from ListOfNumber>
+
+(define (positive-only lon) (filter2 positive? lon))
+
+
+;; ListOfNumber -> ListOfNumber
+;; produce list with only negative? elements of lon
+(check-expect (negative-only empty) empty)
+(check-expect (negative-only (list 1 -2 3 -4)) (list -2 -4))
+
+;(define (negative-only lon) empty) ;stub
+
+;<template from ListOfNumber>
+
+(define (negative-only lon) (filter2 negative? lon))
+
+;; (X -> bool) (listof X) -> (listof X)
+;; return list where each element that fails q is excluded from list
+(check-expect (filter2 positive? empty) empty)
+(check-expect (filter2 positive? (list -1 2 -3 4)) (list 2 4))
+(check-expect (filter2 negative? (list -1 2 -3 4)) (list -1 -3))
+(check-expect (filter2 zero? (list 9 0 1 2)) (list 0))
+(check-expect (filter2 even? (list -9 6 -4 3 2)) (list 6 -4 2))
+
+(define (filter2 q lon)
+  (cond [(empty? lon) empty]
+        [else
+         (if (q (first lon))
+             (cons (first lon)
+                   (filter2 q (rest lon)))
+             (filter2 q (rest lon)))]))
